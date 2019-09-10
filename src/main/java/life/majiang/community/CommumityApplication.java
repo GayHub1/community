b432c7f258cd633ff88a89f6c583254d1ceb4a7c
GayHub1 <942418004@qq.com>
2019-09-10 16:57:32 +0800

    first commit

---
+++ src/main/java/life/majiang/community/CommumityApplication.java
@@ -0,0 +1,12 @@
+package life.majiang.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommumityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommumityApplication.class, args);
+    }
+}
